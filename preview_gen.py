aa28e709cd674b15b447f80deab782f9db173316
n6430 <n643064@tutanota.com>
2023-06-03 02:02:37 +0200

    Stuff

--- preview_gen.py
+++ preview_gen.py
@@ -6,8 +6,11 @@ from os import sep
 from PIL import Image
 
 
+def saturate(c):
+    return c != 0 and 255
 
-def main(world_dir):
+
+def main(world_dir, poi_list_path, output_path):
     if world_dir[len(world_dir) - 1] != sep:
         world_dir += sep
 
@@ -15,31 +18,55 @@ def main(world_dir):
     splat3 = Image.open(world_dir + "splat3.png")
     splat4 = Image.open(world_dir + "splat4.png")
 
-    w = biomes.width
-    h = biomes.height
-    red = splat3.getchannel(0)
-    green = splat3.getchannel(1)
-    blue = splat4.getchannel(2)
+    size = biomes.width
+    center = size/2
+    red = splat3.getchannel(0).point(saturate)
+    green = splat3.getchannel(1).point(saturate)
+    blue = splat4.getchannel(2).point(saturate)
 
-    data = list(zip(red.point(lambda i: i * 5).getdata(), green.point(lambda i: i * 3).getdata(), blue.point(lambda i: i * 7).getdata()))
-    overlay = Image.new("RGBA", (w, h))
+    data = list(zip(red.getdata(), green.getdata(), blue.getdata()))
+    overlay = Image.new("RGBA", (size, size))
     overlay.putdata(data)
     mask = Image.frombytes("RGBA", overlay.size, overlay.tobytes())
     mask = mask.convert("L")
     biomes.paste(overlay, mask=mask)
     tmp = BytesIO()
     biomes.save(tmp, format="PNG")
-
+    tmp.seek(0)
+    tmp.flush()
     background = cairo.ImageSurface.create_from_png(tmp)
     ctx = cairo.Context(background)
-    # TODO:
-    #   - POIs
-    #   - POI color coding
-    #   - Spawnpoints
 
+    prefabs = open(world_dir + "prefabs.xml")
+
+    while True:
+        line = prefabs.readline()
+        if not line or "<prefabs>" in line:
+            break
+    while True:
+        line = prefabs.readline()
+        if not line or "</prefabs>" in line:
+            break
+        line_length = len(line)
+        name_start = line.find("name=") + 6
+        name = line[name_start:line.find("\"", name_start, line_length)]
+        position_start = line.find("position=") + 10
+        position = line[position_start:line.find("\"", position_start, line_length)].split(",")
+        print(name, position)
+        x = center + int(position[0])
+        z = center + int(position[2])
+        print(x, z)
+        ctx.set_source_rgb(0, 0, 0)
+        ctx.rectangle(x-10, z-10, 20, 20)
+        ctx.fill()
+
+    background.flush()
+    background.write_to_png("out.png")
 
 
 if __name__ == "__main__":
     from sys import argv
-
-    main(argv[1])
+    if len(argv) < 4:
+        print("Usage: ./preview_gen [world dir] [poi list] [output file]")
+        exit(1)
+    main(argv[1], argv[2], argv[3])
---
+++ util.py
@@ -0,0 +1,36 @@
+import os
+
+def point(a: list, consumer) -> list:
+    a2 = []
+    for e in a:
+        a2.append(consumer(e))
+    return a2
+
+
+def collect_pois(poi_dir):
+    ls = []
+    for file in os.listdir(poi_dir):
+        if not file.endswith(".xml"):
+            continue
+        fd = open(poi_dir + os.sep + file, "r")
+        while True:
+            line = fd.readline()
+            if not line:
+                break
+            z = line.find("name=\"Zoning\"")
+            if z != -1:
+                l = len(line)
+                v = line.find("value=", z, l) + 7
+                zone = line[v:line.find("\"", v, l)]
+                if zone not in ls:
+                    ls.append(zone)
+                    print(zone)
+
+
+
+
+if __name__ == "__main__":
+    from sys import argv
+    if len(argv) < 2:
+        print("Usage: ./util.py poi_list")
+    collect_pois(argv[1])
